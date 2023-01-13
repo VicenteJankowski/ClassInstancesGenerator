4f664a15e3b75360236e44e8714f7394531a837a
VicenteJankowski <jxstudiopl@gmail.com>
2023-01-13 21:16:48 +0100

    Initial commit

---
+++ src/main/java/pl/admonster/ClassInstancesGenerator/ClassInstancesGenerator.java
@@ -0,0 +1,8 @@
+package pl.admonster.ClassInstancesGenerator;
+
+public class ClassInstancesGenerator {
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+    }
+}
