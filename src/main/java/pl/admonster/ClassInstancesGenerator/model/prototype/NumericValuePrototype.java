439f8f305e6d0a2847a75774e0a1685fe1741895
VicenteJankowski <jxstudiopl@gmail.com>
2023-02-10 00:18:13 +0100

    ADD: Factory method to create NumericValuePrototype instance, made all constructors private, changed default max range value to 100

--- src/main/java/pl/admonster/ClassInstancesGenerator/model/prototype/NumericValuePrototype.java
+++ src/main/java/pl/admonster/ClassInstancesGenerator/model/prototype/NumericValuePrototype.java
@@ -1,5 +1,6 @@
 package pl.admonster.ClassInstancesGenerator.model.prototype;
 
+import pl.admonster.ClassInstancesGenerator.annotation.RangeOfGeneratedValue;
 import pl.admonster.ClassInstancesGenerator.model.Range;
 
 import java.lang.reflect.Field;
@@ -9,13 +10,26 @@ public class NumericValuePrototype extends ValuePrototype {
     private enum DefaultRange {
         ;
         private static final int MIN = 0;
-        private static final int MAX = 1000;
+        private static final int MAX = 100;
     }
 
-    Range numberBoundaries = new Range(DefaultRange.MIN, DefaultRange.MAX);
+    Range numberBoundaries;
 
-    public NumericValuePrototype(Field generationBase) {
+    private NumericValuePrototype(final Field generationBase) {
         super(generationBase);
+    }
+    private NumericValuePrototype(Field generationBase, float minRange, float maxRange) {
+        super(generationBase);
+        setNumberBoundaries(new Range(minRange, maxRange));
+    }
+
+    public static NumericValuePrototype create(Field generationBase) {
+        if(generationBase.isAnnotationPresent(RangeOfGeneratedValue.class))
+            return new NumericValuePrototype(generationBase,
+                                             generationBase.getAnnotation(RangeOfGeneratedValue.class).min(),
+                                             generationBase.getAnnotation(RangeOfGeneratedValue.class).max());
+        else
+            return new NumericValuePrototype(generationBase, DefaultRange.MIN, DefaultRange.MAX);
     }
 
     public Range getNumberBoundaries() {
